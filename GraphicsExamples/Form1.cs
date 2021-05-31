c1bc04b6db22fdfefa29c4dc4f1a17118f9f02db
Tony Theodoropoulos <theodot@outlook.com>
2021-05-31 11:28:02 -0400

    Add project files.

---
+++ GraphicsExamples/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsExamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Text = "Graphics Examples";
+
+            Graphics g = this.CreateGraphics();
+            Pen redPen = new Pen(Color.Red, 3);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            Font drawFont = new Font("Palatino Linotype", 16, FontStyle.Italic);
+
+            g.Clear(Color.White);
+
+            g.DrawLine(redPen, 50, 10, 100, 200);
+
+            g.DrawRectangle(redPen, 30, 30, 100, 200);
+            g.FillRectangle(blueBrush, 30, 30, 100, 200);
+
+            g.DrawEllipse(redPen, 25, 25, 200, 200);
+            g.FillEllipse(blueBrush, 25, 25, 200, 200);
+
+            //g.DrawArc(redPen, 250, 20, 50, 50, 30, 300);
+            //g.DrawPie(redPen, 250, 20, 50, 50, 140, 60);
+            g.FillPie(blueBrush, 250, 20, 50, 50, 120, 300);
+
+            g.DrawString("Begin", drawFont, blueBrush, 50, 300);
+
+        }
+    }
+}
